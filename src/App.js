21f21028b3673be6380729c3738c0e86a5715774
Narmin <nrminseyfullayeva@gmail.com>
2025-08-22 19:03:21 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import { useState } from "react";
 
 function App() {
 
-  const [a, seta] = useState(0)
+  const [a, seta] = useState("Salam mən ") 
+  let b = "Salam mən " , c =b , d = c , e = d 
   return (
     <div className="app">
       <h1>{a}</h1>
-      <button onClick={() => seta((t)=>t+1)}>+</button>
-      <button onClick={() => seta((t)=>t-1)}>-</button>
+      <button onClick={() => seta((t)=>b+"Nərminəm")}>0</button>
+      <button onClick={() => seta((t)=>c+"əlaçıyam")}>1</button> 
+      <button onClick={() => seta((t)=>d+"yaxşı oxuyanam")}>2</button>
+      <button onClick={() => seta((t)=>e+"orta oxuyanam")}>3</button>
     </div>
   );
 }
 
 export default App;
+
