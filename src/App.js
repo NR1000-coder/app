a21e1adfee6e5d1aaca163f6d7184cdd278caf6f
Narmin <nrminseyfullayeva@gmail.com>
2025-08-28 12:28:01 +0000

    /

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import { useState } from "react";
 
-function App() {
+function Me() {
+  const [option, setOption] = useState()
 
-  const [a, seta] = useState("Salam mən ") 
-  let b = "Salam mən " , c =b , d = c , e = d 
   return (
-    <div className="app">
-      <h1>{a}</h1>
-      <button onClick={() => seta((a)=>b+"Nərminəm")}>0</button>
-      <button onClick={() => seta((a)=>c+"əlaçıyam")}>1</button> 
-      <button onClick={() => seta((a)=>d+"yaxşı oxuyanam")}>2</button>
-      <button onClick={() => seta((a)=>e+"orta oxuyanam")}>3</button>
-    </div>
+    <>
+      <h1>Salam mən {option}</h1>
+      <button type="button" 
+      onClick={() => setOption("Nərminəm.")}
+      >0</button>
+      <button type="button"
+      onClick={() => setOption("əlaçıyam.")}
+      >1</button>
+      <button type="button"
+      onClick={() => setOption("yaxşı oxuyanam.")}
+      >2</button>
+      <button type="button"
+      onClick={() => setOption("orta oxuyanam.")}
+      >3</button>
+    </>
   );
 }
 
-export default App;
+export default Me;
 
